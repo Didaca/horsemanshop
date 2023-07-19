336fb8329b6dd125e4663da4562229ec794cff02
Didaca <deni_sk8@abv.bg>
2023-07-19 16:02:59 +0300

    Added validation

--- horsemanshop/shop/serializers.py
+++ horsemanshop/shop/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from horsemanshop.shop.models import Article, Category
 
@@ -36,6 +37,20 @@ class CreateArticleSerializer(serializers.ModelSerializer):
             'description',
             'category'
         ]
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Article.objects.all(),
+                fields=['name', 'article_type'],
+                message='The relation name-type already exist!'
+            )
+        ]
+
+    @staticmethod
+    def validate_name(name):
+        art = Article.objects.filter(name__iexact=name)
+        if art.exists():
+            raise serializers.ValidationError(f'{name} is already exist!')
+        return name
 
 
 class ArticleSerializer(serializers.ModelSerializer):
